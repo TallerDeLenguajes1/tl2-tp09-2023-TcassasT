b22de6c7248f9f98d9a09e6b0922f527e5352f85
Tomás Cassas <tcassas@archt.com.ar>
2023-11-10 19:38:19 -0300

    Organiza repository y controller de usuario.

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -29,7 +29,7 @@ public class UsuarioController: ControllerBase {
   }
 
   [HttpPost("usuario")]
-  public IActionResult PostUsuario(Usuario usuario) {
+  public IActionResult CrearUsuario(Usuario usuario) {
     usuarioRepository.CrearUsuario(usuario);
     return Ok("Usuario creado");
   }
